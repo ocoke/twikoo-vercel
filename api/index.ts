1a3d7006c17ccf2aecbed08f5190af8cb37532fa
CKY <omycoke@outlook.com>
2023-01-09 13:07:55 +0800

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+export default async function (req: VercelRequest, res: VercelResponse) {
+    res.setHeader('JSEW-SERVER', 'Vercel');
+    res.setHeader('access-control-allow-origin', '*');
+    res.send({
+      error: true,
+      messgae: "Twikoo 评论已关闭，请清除缓存后强制刷新页面，切换至 Detalk.js 后重试",
+    });
+}
